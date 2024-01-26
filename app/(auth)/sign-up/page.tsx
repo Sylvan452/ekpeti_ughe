ae6f0f1e1ca39837ce36becc7f4ca8294e69c6cd
Obah Sylva <sylvan452@gmail.com>
2024-01-26 20:45:22 +0100

    authentication fully implimented

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { GithubIcon } from 'lucide-react';
-import GoogleIcon from '../../../public/google.svg';
 import Link from 'next/link';
-import Image from 'next/image';
+import GithubSignInButton from '@/app/components/GithubSignInButton';
+import GoogleSignInButton from '@/app/components/GoogleSignInButton';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '@/app/utils/auth';
 
-export default function SignUp() {
+export default async function SignUp() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    return redirect('/home');
+  }
   return (
     <div className="mt-24 rounded bg-black/70 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
       <form>
@@ -33,12 +40,8 @@ export default function SignUp() {
         </Link>
       </div>
       <div className="flex w-full justify-center items-center gap-3 mt-6">
-        <Button variant="outline" size="icon">
-          <GithubIcon className="w-4 h-4" />
-        </Button>
-        <Button variant="outline" size="icon">
-          <Image src={GoogleIcon} alt="Google Icon" className="w-6 h-6" />
-        </Button>
+        <GithubSignInButton />
+        <GoogleSignInButton />
       </div>
     </div>
   );

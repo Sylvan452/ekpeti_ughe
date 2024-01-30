683289e20d695c971b77219e7698c1211f3db1f9
Obah Sylva <sylvan452@gmail.com>
2024-01-30 17:56:21 +0100

    nav links functionalities implimented

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,13 @@
+import MovieVideo from '../components/MovieVideo';
 import Navbar from '../components/NavBar';
+import RecentlyAdded from '../components/RecentltAdded';
 
 export default function HomePage() {
-  return <h1>Hello Home</h1>;
+  return (
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-2xl font-bold">Recently Added</h1>
+      <RecentlyAdded />
+    </div>
+  );
 }
